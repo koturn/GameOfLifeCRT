af3adc0790e5720130ffbeb121f65848dddf510d
koturn <jeak.koutan.apple@gmail.com>
2022-12-10 18:44:57 +0900

    Add AssemblyInfo.cs

---
+++ Assets/koturn/GameOfLife/Editor/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyCompany("koturn")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCopyright("Copyright (C) 2021 koturn All Rights Reserverd.")]
+[assembly: AssemblyDescription("Material inspectors of koturn/GameOfLife/*.")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: AssemblyProduct("Koturn.GameOfLife")]
+[assembly: AssemblyTitle("Koturn.GameOfLife")]
+[assembly: AssemblyMetadata("RepositoryUrl", "https://github.com/koturn/LillKustomShaders")]
